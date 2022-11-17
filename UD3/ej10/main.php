d113f3b0bf580ce2e3b048f973ccade849073241
Olga Moreno <olga3emes@gmail.com>
2022-11-17 08:34:00 +0100

    ej10

--- UD3/ej10/main.php
+++ UD3/ej10/main.php
@@ -10,6 +10,7 @@ if (!isset($_SESSION['usuario'])) {
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,17 +19,23 @@ if (!isset($_SESSION['usuario'])) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
 </head>
+
 <body>
-    <h1>Bienvenido <?= $_SESSION['usuario'] ?>
-    </h1>
-    <p>Pulse <a href="logout.php">aquí</a> para salir</p>
-    <p>Volver al <a href="main.php">inicio</a></p>
-    <h2>Listado de productos</h2>
-    <ul>
-        <li>Producto 1</li>
-        <li>Producto 2</li>
-        <li>Producto 3</li>
-    </ul>
+    <div class="container-fluid">
+        <div class="row">
+            <h1>Bienvenido <?= $_SESSION['usuario'] ?>
+            </h1>
+            <p class="ml-5">Pulse <a href="logout.php">aquí para salir </a> </p>
+            <p class="ml-5"> Volver
+                    al <a href="main.php">Inicio <i class="bi bi-house"></i></a></p>
+            <h2>Listado de productos<i class="bi bi-cart2"></i></h2>
+            <ul class="align-content-center m-4">
+                <li>Producto 1</li>
+                <li>Producto 2</li>
+                <li>Producto 3</li>
+            </ul>
+        </div>
+    </div>
 </body>
 
 </html>
